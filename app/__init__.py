3321bd3a85a5bae6094b5f680520f40b548d5883
Ananya Ray <ray.ananya.india@gmail.com>
2024-06-26 22:15:12 +0530

    completed assignment

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
+from app.extensions import init_mongo
+from app.webhook.routes import webhook, ui
 
-from app.webhook.routes import webhook
-
-
-# Creating our flask app
 def create_app():
-
     app = Flask(__name__)
-    
-    # registering all the blueprints
-    app.register_blueprint(webhook)
+
+    init_mongo(app)
+
+    app.register_blueprint(webhook, url_prefix='/webhook')
+    app.register_blueprint(ui, url_prefix='/ui')
     
     return app
--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,7 @@
 from flask_pymongo import PyMongo
 
-# Setup MongoDB here
-# mongo = PyMongo(uri="mongodb://localhost:27017/database")
+mongo = PyMongo()
+
+def init_mongo(app):
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/github_webhooks"
+    mongo.init_app(app)
